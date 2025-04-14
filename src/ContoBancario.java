92457c070316dae87c18d171e6c3bfc611b01d7f
adeleBaccelliMeucci <baccelli.adele@itismeucci.com>
2025-04-14 13:47:14 +0200

    doc

--- src/ContoBancario.java
+++ src/ContoBancario.java
@@ -1,7 +1,86 @@
 /**
- * ContoBancario
+ * Classe che rappresenta un conto bancario.
  */
 public class ContoBancario {
+    /**
+     * L'IBAN del conto bancario.
+     */
+    protected String iban;
+    /**
+     * Il saldo del conto bancario.
+     */
+    protected double saldo;
+
+    /**
+     * Costruttore che inizializza il conto bancario con un IBAN e un saldo.
+     * @param iban il numero del conto bancario
+     * @param saldo il saldo iniziale del conto bancario
+     */
+    public ContoBancario(String iban, double saldo) {
+        this.iban = iban;
+        this.saldo = saldo;
+    }
+
+    /**
+     * Deposita una somma di denaro sul conto bancario, incrementando il saldo.
+     * @param importo la somma di denaro da depositare
+     * @throws IllegalArgumentException se l'importo e' negativo
+     */
+    public void deposita(double importo){
+        if (importo < 0 ) {
+            throw new IllegalArgumentException("importo negativo");
+        } else {
+            this.saldo = this.saldo + importo;
+        }
+    }
+
+    /**
+     * Restituisce il numero del conto bancario.
+     * @return il numero del conto bancario
+     */
+    public String getIban() {
+        return iban;
+    }
+
+    /**
+     * Restituisce il saldo del conto bancario.
+     * @return il saldo del conto bancario
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * Preleva una somma di denaro dal conto bancario, decrementando il saldo.
+     * @param importo la somma di denaro da prelevare
+     * @throws IllegalArgumentException se l'importo e' negativo o supera il saldo
+     */
+    public void preleva(double importo){
+        if (importo < 0 && importo > this.saldo) {
+            throw new IllegalArgumentException("importo negatico o supera il saldo");
+        } else {
+            this.saldo = this.saldo - importo;
+        }
+    }
+
+    /**
+     * Restituisce una rappresentazione stringa del conto bancario.
+     * Include l'IBAN e il saldo. La rappresentazione stringa e' formattata per essere facile da leggere e comprendere. Ecco un esempio di come potrebbe apparire la rappresentazione stringa:  <br>
+     * ```  <br>
+     * Conto Bancario:  <br>
+     * IBAN: IT12X0542811101000000123  <br>
+     * Saldo: 1000.0 EUR  <br>
+     * ```  
+     * @Overrides toString in class Object
+     * @return una rappresentazione stringa del conto bancario, inclusivo di IBAN e saldo
+     */
+    @Override
+    public String toString() {
+        return "ContoBancario [iban=" + iban + ", saldo=" + saldo + "]";
+    }
 
     
+    
+    
+
 }
